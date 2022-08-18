fe0402c01d356f6bc3b5815a3ca9c66cbdd61ae3
Guilherme Moraes <guimoraes.dev@gmail.com>
2022-08-18 20:10:14 -0300

    fix: fix sign test

--- src/views/Sign/tests.spec.tsx
+++ src/views/Sign/tests.spec.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import { render } from 'tests/test.utils';
 
-import LoginTemplate from './template';
+import SignTemplate from './template';
 
 const ContainerRender = (mockSubmit: jest.Mock<any, any>) =>
-	render(<LoginTemplate onLoginSuccess={mockSubmit} />);
+	render(
+		<SignTemplate
+			type="signin"
+			title="Welcome to Teller"
+			description="Use one social link above to sign in"
+			handleLoginSuccess={mockSubmit}
+			buttonProps={{
+				label: 'Jest test',
+			}}
+		/>
+	);
 
 describe('Login page', () => {
 	it('should render correctly', () => {
