16a45161aca3f7f9a9fd1307707191d4cf302cc7
Guilherme Moraes <guimoraes.dev@gmail.com>
2022-09-02 12:45:23 -0300

    fix: fix requests without bearer token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import nookies from 'nookies';
 
 if (!process.env.NEXT_PUBLIC_API_URL) {
 	throw new Error('NEXT_PUBLIC_API_URL is missing');
@@ -6,7 +7,23 @@ if (!process.env.NEXT_PUBLIC_API_URL) {
 
 const api = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	
+});
+
+api.interceptors.request.use((config) => {
+	const { token: cookiesToken } = nookies.get();
+
+	if (!cookiesToken) {
+		return config;
+	}
+
+	const token = JSON.parse(cookiesToken) as string;
+
+	return {
+		...config,
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
 });
 
 export default api;
